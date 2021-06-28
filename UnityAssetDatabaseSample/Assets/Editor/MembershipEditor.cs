521c79c77fadac12b3e5d3974f822c5ab5ef2331
superbDerrick <kang.derrick@gmail.com>
2021-06-28 16:41:26 +0900

    added editor codes

---
+++ UnityAssetDatabaseSample/Assets/Editor/MembershipEditor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MembershipEditor : EditorWindow
+{
+    [MenuItem("Membership/StoreSettings")]
+    static void Init()
+    {
+        MembershipEditor window = (MembershipEditor)EditorWindow.GetWindow(typeof(MembershipEditor));
+    }
+
+
+    private const string STORE_AMAZON = "AMAZON";
+    private const string STORE_GOOGLE = "GOOGLE";
+    
+    private void OnGUI()
+    {
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button(STORE_AMAZON))
+        {
+            Debug.Log("AMAZON");
+        }
+ 
+        if (GUILayout.Button(STORE_GOOGLE))
+        {
+            Debug.Log("GOOGLE");
+        }
+        
+        EditorGUILayout.EndHorizontal();
+    }
+}
